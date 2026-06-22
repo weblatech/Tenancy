7f17f9f798b3131d72fe7b09692d2aac086d510d
weblatech <teddyforu2000@gmail.com>
2026-06-22 15:35:14 +0500

    fix: 419 CSRF - exempt admin/webhook routes (protected by auth middleware)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            'admin/*',
+            'webhook/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
