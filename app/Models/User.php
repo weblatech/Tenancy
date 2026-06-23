8f919e70bce0f027b171e221f1c0661593e80304
weblatech <teddyforu2000@gmail.com>
2026-06-23 16:55:01 +0500

    fix: User model always uses central pgsql connection

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $connection = 'pgsql';
+
     /**
      * The attributes that are mass assignable.
      *
