4312b4885379617d69e66e392ea0aef91f88ab99
weblatech <teddyforu2000@gmail.com>
2026-06-23 16:55:35 +0500

    fix: User model uses central connection from config

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    protected $connection = 'pgsql';
+    protected $connection = config('tenancy.database.central_connection', 'pgsql');
 
     /**
      * The attributes that are mass assignable.
